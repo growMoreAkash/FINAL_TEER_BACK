83d1a82011f928e35dabbe435c077ce1efee0401
growMoreAkash <akashgrowmore@gmail.com>
2024-03-04 17:57:12 +0530

    final

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import ENV from '../config.js'
 import 'dotenv/config'
 
-const DB = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.pkemegp.mongodb.net/Teer_db?retryWrites=true&w=majority&appName=Cluster0`
+const DB = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ujfba7i.mongodb.net/Teer_db?retryWrites=true&w=majority&appName=Cluster0`
 
 const connect = async () => {
     const database = await mongoose.connect(DB, {
